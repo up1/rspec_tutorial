7d2f705131d83ccc618f0052b41b55aa7747b7ce
Somkiat <somkiat.p@gmail.com>
2014-04-22 01:54:46 +0700

    Remove duplicate with subject and before

--- spec/burger_spec.rb
+++ spec/burger_spec.rb
@@ -4,26 +4,22 @@ require 'spec_helper'
 describe Burger do
 
 	describe "#apply_ketchup" do
-		
-		context  "with ketchup" do
-			before do
-				@burger = Burger.new(:ketchup => true)
-				@burger.apply_ketchup
-			end
+		subject { burger }
+		before  { burger.apply_ketchup }
 
+		context  "with ketchup" do
+			let(:burger) { Burger.new(:ketchup => true) }
+			
 			it "sets the ketchup flag to true" do		
-				@burger.has_ketchup_on_it?.should be_true
+				burger.has_ketchup_on_it?.should be_true
 			end
 		end
 
 		context  "without ketchup" do
-			before do
-				@burger = Burger.new(:ketchup => false)
-				@burger.apply_ketchup
-			end
+			let(:burger) { Burger.new(:ketchup => false) }
 
 			it "sets the ketchup flag to false" do				
-				@burger.has_ketchup_on_it?.should be_false
+				burger.has_ketchup_on_it?.should be_false
 			end
 		end
 	end
