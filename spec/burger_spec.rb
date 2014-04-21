701d6320f9a38cf7e391cb31fac6b5fd2171fe30
Somkiat <somkiat.p@gmail.com>
2014-04-22 01:44:16 +0700

    Refactor with before

--- spec/burger_spec.rb
+++ spec/burger_spec.rb
@@ -4,18 +4,25 @@ require 'spec_helper'
 describe Burger do
 
 	describe "#apply_ketchup" do
+		
 		context  "with ketchup" do
-			it "sets the ketchup flag to true" do
+			before do
 				@burger = Burger.new(:ketchup => true)
 				@burger.apply_ketchup
+			end
+
+			it "sets the ketchup flag to true" do		
 				@burger.has_ketchup_on_it?.should be_true
 			end
 		end
 
 		context  "without ketchup" do
-			it "sets the ketchup flag to false" do
+			before do
 				@burger = Burger.new(:ketchup => false)
 				@burger.apply_ketchup
+			end
+
+			it "sets the ketchup flag to false" do				
 				@burger.has_ketchup_on_it?.should be_false
 			end
 		end
